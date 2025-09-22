c0876dbba47d0b9e65273ac21e49947d9b6786f7
AltoDev <hitokawiswara@gmail.com>
2025-09-22 16:06:27 +0700

    Commit No. 2 - Parse JSON with Moshi and Display Image URL

--- app/src/main/java/umn/ac/id/lab_week_05/api/CatApiService.kt
+++ app/src/main/java/umn/ac/id/lab_week_05/api/CatApiService.kt
@@ -3,11 +3,12 @@ package umn.ac.id.lab_week_05.api
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
+import umn.ac.id.lab_week_05.model.ImageData
 
 interface CatApiService {
     @GET("images/search")
     fun searchImages(
         @Query("limit") limit: Int,
         @Query("size") format: String
-    ) : Call<String>
+    ) : Call<List<ImageData>>
 }
---
+++ app/src/main/java/umn/ac/id/lab_week_05/model/CatBreedData.kt
@@ -0,0 +1,6 @@
+package umn.ac.id.lab_week_05.model
+
+data class CatBreedData(
+    val name: String,
+    val temperament: String
+)
